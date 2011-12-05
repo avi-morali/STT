61c509ad58ce95805e5dbe2866e5d9dc3328e014
avimo <avi.morali@gmail.com>
2011-12-05 22:04:46 +0200

    The first project

---
+++ STT/STT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="STT.MvcApplication" Language="C#" %>
---
+++ STT/STT/Models/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STT.Models
+{
+    public class Message
+    {
+        private int Id;
+        private string Content;
+        private DateTime Date;
+
+        public Message()
+        {
+            Id = 0;
+            Content = "";
+            Date = DateTime.Now;
+        }
+
+        public Message(int id, string content, string date)
+        {
+            IFormatProvider fp = new System.Globalization.CultureInfo("en-US");
+
+            Id = id;
+            Content = content;
+            Date = DateTime.ParseExact(date, "MM/dd/yyyy", fp);
+
+        }
+
+        public void printMessage()
+        {
+        }
+    }
+}
