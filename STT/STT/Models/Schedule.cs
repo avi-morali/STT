6c6c5c1ae967624781fe645d38e78a5553a438ec
dimagr <dimaitrius@gmail.com>
2011-12-11 20:24:01 +0200

    Schedule class

---
+++ STT/STT/Models/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STT.Models
+{
+    public class Schedule
+    {
+
+        private int[][] table;
+        private long Student_ID;
+
+        public Schedule()
+        {
+            this.table = new int[14][];
+            for (int i = 0; i < 14; i++)
+            {
+                table[i] = new int[6];
+            }
+            Student_ID = -1;
+        }
+        public Schedule(long id, int rows)
+        {
+            this.table = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                table[i] = new int[6];
+            }
+            Student_ID = id;
+        }
+
+    }
+}
