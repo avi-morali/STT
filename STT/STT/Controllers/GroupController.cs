ffe2ee98dec1df3a3a3eb04bf5a45afd120d1022
pinibn <pinibn@gmail.com>
2011-12-13 17:40:00 +0200

    controller

---
+++ STT/STT/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STT.Controllers
+{
+    public class GroupController : Controller
+    {
+        //
+        // GET: /Group/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
