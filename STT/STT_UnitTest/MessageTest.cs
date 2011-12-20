37167776cb7662bf75ef1897680deaae58a89198
avimo <avi.morali@gmail.com>
2011-12-21 00:38:09 +0200

    fix the date problem

--- STT/STT_UnitTest/MessageTest.cs
+++ STT/STT_UnitTest/MessageTest.cs
@@ -15,7 +15,7 @@ namespace STT_UnitTest
             int id = 0;
             string content = "";
             string date = DateTime.Today.ToString();
-            Message tar1 = new Message(id, content, "20/12/2011");
+            Message tar1 = new Message(id, content, date);
             Message tar2 = new Message();
             Assert.AreEqual(tar1.getId(), tar2.getId(),"fail id");
             Assert.AreEqual(tar1.getContent(), tar2.getContent(), "fail content");
