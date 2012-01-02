a67d52171bcdfac1be420055975959e4b2d121e2
avimo <avi.morali@gmail.com>
2012-01-02 13:12:46 +0200

    enter data to the database and build the DBConnection class

---
+++ STT/STT/Models/DBConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace STT.Models
+{
+    public class DBConnection
+    {
+        private SqlConnection connection = null;
+
+
+        public DBConnection()
+        {
+            string connectionString = "Server=.\\SQLExpress;AttachDbFilename=D:\\לימודים\\הנדסת תוכנה\\שנה ג\\סמסטר ה\\הנדסת תוכנה\\STT\\Code\\JceDB.mdf;Database=JceDB; Trusted_Connection=Yes;";
+            //string connectionString = "Server=.\\SQLExpress;AttachDbFilename=..//..//..//JceDB.mdf;Database=JceDB; Trusted_Connection=Yes;";
+            
+            //try
+            //{
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+            /*}
+            catch (Exception ex)
+            {
+                if (connection != null)
+                {
+                    connection.Dispose();
+                }
+            }*/
+        }
+
+        public SqlDataReader queryExexute(string sql)
+        {
+            SqlCommand sqlline = new SqlCommand(sql, connection);
+            return sqlline.ExecuteReader();
+        }
+    }
+}
